217dcb9708d66f80417b3f51d91e009b5706b722
Victor Manuel <vicmandlagasca@gmail.com>
2017-08-25 13:54:23 -0500

    Create a HTTPS server localhost and palce a config.json

--- main.go
+++ main.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 	"log"
+	"io/ioutil"
+	"encoding/json"
 )
+var p = fmt.Println
+var pf = fmt.Printf
+
+// Estructura Configuration
+type Config struct {
+	Port 	string `json:"port"`
+	CertPem string `json:"cert_pem"`
+	KeyPem 	string `json:"key_pem"`
+}
+
+var config Config
 
 func main() {
+	readConfig()
+
 	http.HandleFunc("/", saludar)
-	log.Println("Servidor iniciado http://knowblyhost:9087")
-	log.Println(http.ListenAndServe(":8087", nil))
+	pf("Servidor iniciado https://knowblyhost:%s", config.Port)
+	err := http.ListenAndServeTLS(config.Port, config.CertPem, config.KeyPem, nil)
+	if err != nil {
+		p(err)
+	}
+
 }
 
 func saludar(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("HTtp service with golang"))
+}
+
+func readConfig() {
+	p("Leyendo el archivo de configuracion....")
+	b, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatalf("No se pudo leer la configuracion inicial: %v", err)
+	}
+
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		log.Fatalf("Error al convertir el archivo:  %v", err)
+	}
+	p("Archivo leido")
 }
